46a6ef9e545c03fa77c1869ed1599a371fdd278a
Carlos Gabriel Correia Neubert <carlos.neubert@rede.ulbra.br>
2026-05-18 11:53:09 -0300

    meu primeirocommit

---
+++ Filme.cs
@@ -0,0 +1,12 @@
+
+public class Filme
+{
+    public int Id{get; set;}
+    public string Titulo{get; set;} = "";
+
+    public string Genero{get; set;} = "";
+
+    public DateTime lancamento{get; set;}
+    public bool Disponivel{get;set;}
+    
+}
---
+++ Program.cs
@@ -0,0 +1,124 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<AppDbContext>(options 
+=> options.UseSqlite("Data Source=Filme.db"));
+
+var app = builder.Build();
+
+
+
+
+
+using (var scope = app.Services.CreateAsyncScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService< AppDbContext> ();
+    db.Database.EnsureCreated();
+
+  
+    
+}
+
+
+app.MapGet("/", () => new
+{
+    status = "online",
+    mensagem = "API Funcionando",
+    dataHora = DateTime.Now
+
+
+});
+
+
+
+app.MapGet("/filme", async (AppDbContext db) => 
+
+{
+    var filmes = await db.Filme.ToListAsync();
+     return Results.Ok(filmes);
+
+
+});
+
+
+app.MapGet("/filme/{id:int}", async (int id, AppDbContext db)  => 
+{
+
+    var filmes = await db.Filme.FindAsync(id);
+
+    if(filmes == null){
+        return Results.NotFound();
+    }
+
+
+    return Results.Ok(filmes);
+
+});
+
+app.MapGet("filme/{titulo}", async(string titulo, AppDbContext db)=>{
+
+
+    var filme = await db.Filme.FirstOrDefaultAsync(f => f.Titulo == titulo);
+    if(filme == null)
+        return Results.NotFound();
+    
+
+    return Results.Ok(filme);
+
+
+
+
+});
+
+
+app.MapPost("/filme", async (Filme filmeReq, AppDbContext db) =>
+{
+   db.Filme.Add(filmeReq);
+   await db.SaveChangesAsync();
+   return Results.Created($"/filme/{filmeReq.Id}",filmeReq);
+
+});
+
+app.MapPut("/filme/{id:int}", async (int id, Filme filmeAtualizado, AppDbContext db)=>{
+
+    var filme = await db.Filme.FindAsync(id);
+
+
+    if(filme == null)
+    
+        return Results.NotFound();
+    
+
+    filme.Titulo = filmeAtualizado.Titulo;
+    filme.Genero = filmeAtualizado.Genero;
+    filme.lancamento = filmeAtualizado.lancamento;
+    filme.Disponivel = filmeAtualizado.Disponivel;
+
+    await db.SaveChangesAsync();
+
+    return Results.Ok(filme);
+
+    
+});
+
+    app.MapDelete("/filme/{id:int}", async (int id, AppDbContext db)=>{
+        
+        var filme = await db.Filme.FindAsync(id);
+
+        if(filme == null)
+        return Results.NotFound();
+
+        db.Remove(filme);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+
+
+});
+
+
+
+app.Run();
+
+
